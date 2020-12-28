fb4c4d710a53dbd4996ab9ca7c1fbe535d86bfb6
R C N <plbwymw@gmail.com>
2020-12-28 22:25:28 +0200

    noscript?

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,6 +91,14 @@
                     Laravel & React
                 </div>
 
+
+
+                <noscript>
+                    For full functionality of this site it is necessary to enable JavaScript.
+                    Here are the <a href="https://www.enable-javascript.com/">
+                    instructions how to enable JavaScript in your web browser</a>.
+                   </noscript>
+                   
                 <div id="welcome"></div>
 
             </div>
